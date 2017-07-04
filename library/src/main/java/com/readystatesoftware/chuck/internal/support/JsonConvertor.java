675065f7aaa3d01484bcee6c5c4f7ba30d47e395
xuzhenzhou <xuzhenzhou@iyunxiao.com>
2017-07-04 17:43:25 +0800

    toJson 转换添加disableHtmlEscaping

--- library/src/main/java/com/readystatesoftware/chuck/internal/support/JsonConvertor.java
+++ library/src/main/java/com/readystatesoftware/chuck/internal/support/JsonConvertor.java
@@ -35,6 +35,7 @@ public class JsonConvertor {
                     .setPrettyPrinting()
                     .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
                     .registerTypeAdapter(Date.class, new DateTypeAdapter())
+                    .disableHtmlEscaping()
                     .create();
         }
         return gson;
